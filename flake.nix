7d56593a277cb6498886d9ff4af72970dd0c22e0
Matias Yocca <matias@yocca.dev>
2025-04-19 04:06:16 -0300

    feat: setup nix development environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs {
+            inherit system;
+        });
+        php = pkgs.php.buildEnv {
+            extensions = { enabled, all }: enabled ++ (with all; [ swoole ]);
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                nodejs
+                php
+                php.packages.composer
+            ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
