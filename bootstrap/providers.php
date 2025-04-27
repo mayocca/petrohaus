6d97f4d599122e2358df5a689f899e9f4c9be873
Matias Yocca <matias@yocca.dev>
2025-04-27 18:26:13 -0300

    refactor: remove filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FolioServiceProvider::class,
-    
+
     App\Modules\Manifest\Providers\ManifestProvider::class,
 ];
